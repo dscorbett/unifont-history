a3877488eb4141e161f2888cd1ff3f626188a0dd
David Corbett <corbett.dav@northeastern.edu>
2025-02-25 17:57:36 -0500

    Finishing unpacking 16.0.02

---
+++ unifont/unifont.tar.gz.d/unifont/doxygen/html/struct_glyph.js
@@ -0,0 +1,9 @@
+var struct_glyph =
+[
+    [ "bitmap", "struct_glyph.html#abbbc1113c6e10dc0fa986abbb7f7665e", null ],
+    [ "byteCount", "struct_glyph.html#aa309ceb984d5524ef7dab0e8927fdd64", null ],
+    [ "codePoint", "struct_glyph.html#a08ed20f3df1902ce776f6e1fccd089b9", null ],
+    [ "combining", "struct_glyph.html#ada6b8b4b9419096c413b5a29dbc54aac", null ],
+    [ "lsb", "struct_glyph.html#a7fb4dc03b71777de880164eee4ffbc51", null ],
+    [ "pos", "struct_glyph.html#ac113da35f4193641855eaf51547f9b43", null ]
+];
